31c6aa7c0b430ff980c91181e90a1a23de6759c1
Young <sean0722@msn.com>
2014-09-13 16:45:16 +0800

    RBAC_TEST

--- application/controllers/rbac_test.php
+++ application/controllers/rbac_test.php
@@ -10,34 +10,40 @@ class Rbac_test extends CI_Controller
 	public function index ()
 	{
 
-		$id=1;
-		$v='add';
-
 		header("content-type:text/html;charset=utf-8");
 		// 创建一个操作
 		$cp = $this->rbac->create_operation("add", "新建 页面");
-		var_dump($cp);
 
 		// 创建角色
 		$admin 	= $this->rbac->create_role("系统管理员", "系统管理 角色");
-		var_dump($admin);
 
 		// 给角色授权
 		$this->rbac->add_childs($admin, array($cp));
 
 		// 分配角色给用户
-		$this->rbac->assign("系统管理员", $id); //admin
+		$this->rbac->assign("系统管理员", 1); //admin
 
 		$this->output->enable_profiler(TRUE);
-
+		$operation = 'add';
 		// 验证权限
-		if ($this->rbac->check_user_access($id, $v))
+		if ($this->rbac->check_user_access(1, $operation))
 		{
-			echo "<span style='color:green'>有权限 $v</span>";
+			echo "<span style='color:green'>有权限  $operation</span>";
 		}
 		else
 		{
-			echo "<span style='color:red'>无权限 $v</span>";
+			echo "<span style='color:red'>无权限  $operation</span>";
 		}
+		echo "<hr>";
+		$operation = 'edit';
+		if ($this->rbac->check_user_access(1, $operation))
+		{
+			echo "<span style='color:green'>有权限  $operation</span>";
+		}
+		else
+		{
+			echo "<span style='color:red'>无权限  $operation</span>";
+		}
+
 	}
 }
--- application/models/rbac_model.php
+++ application/models/rbac_model.php
@@ -5,7 +5,7 @@ class Rbac_model extends CI_Model {
     const TYPE_OPERATION = 0;
     const TYPE_TASK = 1;
     const TYPE_ROLE = 2;
-    
+
     private $_table;
     private $_childs_table;
     private $_assignments_table;
@@ -17,41 +17,41 @@ class Rbac_model extends CI_Model {
     	$this->_initialize();
 	$this->load->database();
     }
-    
+
     private function _initialize() {
     	($this->config->item('table'))				|| $this->_table = 'auth_item';
     	($this->config->item('childs_table'))		|| $this->_primary_key = 'id';
     	($this->config->item('assignments_table'))	|| $this->_childs_table = 'auth_item_child';
     	($this->config->item('primary_key'))		|| $this->_assignments_table = 'auth_assignment';
     }
-    
-    function get_item_by_pk($id) 
+
+    function get_item_by_pk($id)
     {
         return $this->db->get_where($this->_table, array($this->_primary_key => $id))->row();
     }
-    
+
     function get_item_by_name($name)
     {
         return $this->db->get_where($this->_table, array('name' => $name))->row();
     }
-    
-    function create_item($name, $type, $description='', $biz_rule='', $data='') 
+
+    function create_item($name, $type, $description='', $biz_rule='', $data='')
     {
         $conditions = array('name' => $name, 'type' => $type, 'description' => $description, 'bizRule' => $biz_rule, 'data' => $data);
-        
+
         $q = $this->db->get_where($this->_table, $conditions);
-        
+
         if ($q->num_rows() === 0)
         {
             $this->db->insert($this->_table, $conditions);
             return $this->db->insert_id();
-        } 
+        }
         else
         {
             return $q->row()->id;
         }
     }
-    
+
     function add_child($parent, $child)
     {
         if ($parent == $child)
@@ -67,27 +67,27 @@ class Rbac_model extends CI_Model {
             return $this->db->insert_id();
         }
     }
-    
+
     function add_childs($parent, $array_childs)
     {
         foreach ($array_childs as $child)
         {
             $this->add_child($parent, $child);
-        } 
+        }
     }
-    
+
     function item_has_child($item, $child)
     {
         $this->db->where(array('parent_id' => $item, 'child_id' => $child));
         return ($this->db->count_all_results($this->_childs_table) ? TRUE : FALSE);
     }
-    
+
     function has_childs($item)
     {
         $this->db->where(array('parent_id' => $item));
         return ($this->db->count_all_results($this->_childs_table) ? TRUE : FALSE);
     }
-    
+
     function get_item_childs($item)
     {
         $this->db->select("{$this->_table}.*");
@@ -95,7 +95,7 @@ class Rbac_model extends CI_Model {
         $this->db->join("{$this->_childs_table}", "{$this->_table}.id={$this->_childs_table}.child_id");
         return $this->db->get($this->_table)->result();
     }
-    
+
     function get_inherited_roles($itemid)
     {
         $this->db->select("{$this->_table}.*");
@@ -104,7 +104,7 @@ class Rbac_model extends CI_Model {
         $this->db->join("{$this->_childs_table}", "{$this->_table}.id={$this->_childs_table}.child_id");
         return $this->db->get($this->_table)->result();
     }
-    
+
     function check_access($item_name, $childname)
     {
         if ($item = $this->get_item_by_name($item_name) && $child = $this->get_item_by_name($childname))
@@ -113,25 +113,25 @@ class Rbac_model extends CI_Model {
         }
 
     }
-    
+
     function check_access_by_id($itemid, $somethingid)
     {
         if ($this->item_has_child($itemid, $somethingid))
         {
             return TRUE;
         }
-        
-        if ($roles = $this->get_inherited_roles($itemid)) 
+
+        if ($roles = $this->get_inherited_roles($itemid))
         {
-            foreach ($roles as $role) 
+            foreach ($roles as $role)
             {
                 return $this->check_access_by_id($role->id, $somethingid);
             }
         }
         return FALSE;
     }
-    
-    function check_user_access($uid, $item_name) 
+
+    function check_user_access($uid, $item_name)
     {
         if ($item = $this->get_item_by_name($item_name))
         {
@@ -139,7 +139,7 @@ class Rbac_model extends CI_Model {
         }
         return FALSE;
     }
-    
+
     function check_user_access_id($uid, $itemid)
     {
         if ($this->user_has_item($uid, $itemid))
@@ -156,13 +156,13 @@ class Rbac_model extends CI_Model {
         }
         return FALSE;
     }
-    
+
     function user_has_item($uid, $itemid)
     {
         $this->db->where(array('userid' => $uid, 'itemid' => $itemid));
         return ($this->db->count_all_results($this->_assignments_table) ? TRUE : FALSE);
     }
-    
+
     function get_assignments($uid)
     {
         $this->db->select("{$this->_table}.*");
@@ -170,7 +170,7 @@ class Rbac_model extends CI_Model {
         $this->db->where('userid', $uid);
         return $this->db->get($this->_table)->result();
     }
-    
+
     function assign($item_name,$user_id, $biz_rule='',$data='')
     {
         if ($item = $this->get_item_by_name($item_name))
@@ -178,12 +178,12 @@ class Rbac_model extends CI_Model {
         	return $this->assign_by_id($item->id, $user_id, $biz_rule, $data);
         }
     }
-    
+
     function assign_by_id($itemId,$user_id, $biz_rule='',$data='')
     {
         $conditions = array('itemid' => $itemId, 'userid' => $user_id, 'bizrule' => $biz_rule, 'data' => $data);
         $q = $this->db->get_where($this->_assignments_table, $conditions);
-        
+
         if ($q->num_rows() === 0)
         {
             $this->db->insert($this->_assignments_table, $conditions);
@@ -191,33 +191,33 @@ class Rbac_model extends CI_Model {
         }
         return $q->row()->id;
     }
-    
-    function create_role($name, $description='', $biz_rule=NULL, $data=NULL) 
+
+    function create_role($name, $description='', $biz_rule=NULL, $data=NULL)
     {
         return $this->create_item($name, self::TYPE_ROLE, $description, $biz_rule, $data);
     }
 
-    function create_task($name, $description='', $biz_rule=NULL, $data=NULL) 
+    function create_task($name, $description='', $biz_rule=NULL, $data=NULL)
     {
         return $this->create_item($name, self::TYPE_TASK, $description, $biz_rule, $data);
     }
 
-    function create_operation($name, $description='', $biz_rule=NULL, $data=NULL) 
+    function create_operation($name, $description='', $biz_rule=NULL, $data=NULL)
     {
         return $this->create_item($name, self::TYPE_OPERATION, $description, $biz_rule, $data);
     }
-    
-    function get_roles() 
+
+    function get_roles()
     {
         return $this->db->get_where($this->_table, array('type' => self::TYPE_ROLE))->result();
     }
 
-    function get_tasks() 
+    function get_tasks()
     {
 		return $this->db->get_where($this->_table, array('type' => self::TYPE_TASK))->result();
     }
 
-    function get_operations() 
+    function get_operations()
     {
         return $this->db->get_where($this->_table, array('type' => self::TYPE_OPERATION))->result();
     }
@@ -234,7 +234,7 @@ class Rbac_model extends CI_Model {
     			if (!$this->_is_duplicated_operation($item, $array) && $this->_is_operation($item))
     			{
     				$array[] = $item->name;
-    			}    			
+    			}
     			$this->get_item_operations($item->child_id, $array);
     		}
     	} else {
@@ -244,26 +244,26 @@ class Rbac_model extends CI_Model {
     			{
     				$array[] = $item->name;
     			}
-    			
+
     		}
-    	}		
+    	}
 	}
-	
+
 	function get_user_operations($uid, &$array)
 	{
-		foreach($this->db->get_where('auth_assignment', array('userid' => $uid))->result() as $item) 
+		foreach($this->db->get_where('auth_assignment', array('userid' => $uid))->result() as $item)
 		{
 			$this->get_item_operations($item->itemid, $array);
 		}
 	}
-	
+
 	private function _is_duplicated_operation($item, $array) {
 		return (in_array($item->name, $array));
 	}
-	
+
 	private function _is_operation($item) {
 		return (!in_array($item->type, array(self::TYPE_ROLE, self::TYPE_TASK)));
 	}
-	
+
 }
 
